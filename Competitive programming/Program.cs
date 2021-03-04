988e03501988ed04aeff4f078e8a09b5d791e36f
none <moeez_agha@outlook.com>
2021-03-03 22:55:32 -0800

    Update problem statement and summary

--- Competitive programming/Program.cs
+++ Competitive programming/Program.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 
 namespace Competitive_programming
 {
+    /// <summary>
+    /// There are many different solution routes,
+    /// each of which involves a different technique.This handout details the problem and gives
+    /// a few different solution routes.
+    /// 
+    /// Problem Statement:
+    /// 
+    /// You are given an array of n integers and a number k.Determine whether there is a pair
+    /// of elements in the array that sums to exactly k.For example, given the array[1, 3, 7] and
+    /// k = 8, the answer is “yes,” but given k = 6 the answer is “no.”
+    ///</summary>
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = { 9, 5, 1, 23 };
-            var result = TwoNumberSum(array, 10);
+            int[] array = { 1, 3, 7 };
+            var result = TwoNumberSum(array, 8);
             Console.WriteLine($"{result[0]},{result[1]}");
         }
         public static int[] TwoNumberSum(int[] array, int target)
